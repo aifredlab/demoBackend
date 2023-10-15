9662623daaa512e24dcfef6a75e812d999d95789
Jungmin, Kim <jmkim7707@gmail.com>
2023-10-15 23:06:37 +0900

    움..별거없음..

--- src/main/java/com/aifred/controller/LlmController.java
+++ src/main/java/com/aifred/controller/LlmController.java
@@ -1,19 +1,12 @@
 package com.aifred.controller;
 
-import com.aifred.dto.Hello;
 import com.aifred.dto.QuestionReply;
-import com.aifred.entity.InsCompany;
-import com.aifred.entity.Insurance;
-import com.aifred.service.InsuranceService;
+import com.aifred.dto.QuestionRequest;
 import com.aifred.service.LlmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -23,9 +16,15 @@ public class LlmController {
 	@Autowired
 	LlmService llmService;
 	
+	@PostMapping("/ask")
+	public ResponseEntity<QuestionReply> ask(@RequestParam QuestionRequest questionRequest) throws Exception {
+		QuestionReply questionReply =  llmService.ask(questionRequest.getQuestion());
+		return new ResponseEntity<QuestionReply>(questionReply, HttpStatus.OK);
+	}
+
 	@GetMapping("/ask")
-	public ResponseEntity<QuestionReply> ask(String question) throws Exception {
-		QuestionReply questionReply =  llmService.ask(question);
+	public ResponseEntity<QuestionReply> ask2(@RequestParam QuestionRequest questionRequest) throws Exception {
+		QuestionReply questionReply =  llmService.ask(questionRequest.getQuestion());
 		return new ResponseEntity<QuestionReply>(questionReply, HttpStatus.OK);
 	}
 
---
+++ src/main/java/com/aifred/dto/QuestionRequest.java
@@ -0,0 +1,13 @@
+package com.aifred.dto;
+
+public class QuestionRequest {
+    String question;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
